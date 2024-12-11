84ecacb8731b46a10f6e30af27e1521bb46461c3
AzimjonNurnazarov <azimn1993@gmail.com>
2024-12-11 15:52:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,61 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header className='shadow'>
+        <div className='container-fluid py-3'>
+          <img src="https://olcha.uz/_nuxt/plus.lRzD4Jf7.png" alt="" />
+        </div>
       </header>
+      <section id='products' className='py-5'>
+        <div className='container-fluid'>
+          <div className='row'>
+              <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+                <div className='productCard p-3  '>
+                  <img className='d-block productImg' src="https://olcha.uz/image/266x355/products/cdn_1/supplier/stores/1/2024-11-28/PK439EIN2oVAvX6ezVPJYOJF8X5v1kzGlRvGhIRy7Nwht9h7qbyRFx89dDVn.png" alt="" />
+                  <a className='groupName' href="https://olcha.uz/oz/product/view/apple-iphone-15-pro-max">Smartfonlar</a>
+                  <p className='price'>16 161 578 so'm</p> 
+                  <p className='priceMonth'>1 893 000 so'm / 12 oy.</p>
+                  <p className='title'>Apple iPhone 15 Pro Max</p>
+                  <a className='btn' href="https://olcha.uz/oz"><button className='btn'>Muddatli to'lov</button></a>
+                </div>
+                <div className='productCard'>
+                  <img className='d-block productImg' src="https://olcha.uz/image/266x355/products/cdn_1/supplier/stores/1/2024-11-28/PK439EIN2oVAvX6ezVPJYOJF8X5v1kzGlRvGhIRy7Nwht9h7qbyRFx89dDVn.png" alt="" />
+                  <a className='groupName d-block mb-2 mt-3' href="https://olcha.uz/oz/product/view/apple-iphone-15-pro-max">Smartfonlar</a>
+                  <p className='price'>16 161 578 so'm</p> 
+                  <p className='priceMonth'>1 893 000 so'm / 12 oy.</p>
+                  <p className='title'>Apple iPhone 15 Pro Max</p>
+                  <a className='btn' href="https://olcha.uz/oz"><button className='btn'>Muddatli to'lov</button></a>
+                </div>
+                <div className='productCard'>
+                  <img className='d-block productImg' src="https://olcha.uz/image/266x355/products/cdn_1/supplier/stores/1/2024-11-28/PK439EIN2oVAvX6ezVPJYOJF8X5v1kzGlRvGhIRy7Nwht9h7qbyRFx89dDVn.png" alt="" />
+                  <a className='groupName' href="https://olcha.uz/oz/product/view/apple-iphone-15-pro-max">Smartfonlar</a>
+                  <p className='price'>16 161 578 so'm</p> 
+                  <p className='priceMonth'>1 893 000 so'm / 12 oy.</p>
+                  <p className='title'>Apple iPhone 15 Pro Max</p>
+                  <a className='btn' href="https://olcha.uz/oz"><button className='btn'>Muddatli to'lov</button></a>
+                </div>
+                <div className='productCard'>
+                  <img className='d-block productImg' src="https://olcha.uz/image/266x355/products/cdn_1/supplier/stores/1/2024-11-28/PK439EIN2oVAvX6ezVPJYOJF8X5v1kzGlRvGhIRy7Nwht9h7qbyRFx89dDVn.png" alt="" />
+                  <a className='groupName' href="https://olcha.uz/oz/product/view/apple-iphone-15-pro-max">Smartfonlar</a>
+                  <p className='price'>16 161 578 so'm</p> 
+                  <p className='priceMonth'>1 893 000 so'm / 12 oy.</p>
+                  <p className='title'>Apple iPhone 15 Pro Max</p>
+                  <a className='btn' href="https://olcha.uz/oz"><button className='btn'>Muddatli to'lov</button></a>
+                </div>
+                <div className='productCard'>
+                  <img className='d-block productImg' src="https://olcha.uz/image/266x355/products/cdn_1/supplier/stores/1/2024-11-28/PK439EIN2oVAvX6ezVPJYOJF8X5v1kzGlRvGhIRy7Nwht9h7qbyRFx89dDVn.png" alt="" />
+                  <a className='groupName' href="https://olcha.uz/oz/product/view/apple-iphone-15-pro-max">Smartfonlar</a>
+                  <p className='price'>16 161 578 so'm</p> 
+                  <p className='priceMonth'>1 893 000 so'm / 12 oy.</p>
+                  <p className='title'>Apple iPhone 15 Pro Max</p>
+                  <a className='btn' href="https://olcha.uz/oz"><button className='btn'>Muddatli to'lov</button></a>
+                </div>
+              </div>
+          </div>
+        </div>
+
+      </section>
     </div>
   );
 }
